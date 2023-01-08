3a27a557498c2cc23850b04819d63531c6579e6d
Adej0la <recursr.dev@gmail.com>
2023-01-08 09:15:25 +0100

    Add C script that checks for cycle in singly linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,20 +2,18 @@
 
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
- * list: A pointer to a list
+ * @list: A pointer to a list
  *
  * Return: 0 if no cycle, 1 if cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *fastPtr, *slowPtr;
+	listint_t *fastPtr = list, *slowPtr = list;
 
-	fastPtr = list, slowPtr = list;
-
-	while(fastPtr && slowPtr && fastPtr -> next)
+	while (fastPtr && slowPtr && fastPtr->next == NULL)
 	{
-		slowPtr = list -> next;
-		fastPtr = list -> next -> next;
+		slowPtr = slowPtr->next;
+		fastPtr = fastPtr->next->next;
 
 		if (slowPtr == fastPtr)
 			return (1);
