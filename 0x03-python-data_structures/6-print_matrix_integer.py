5f9ae199e52b2755d74eb1a3605b93de8d2ae467
Adej0la <recursr.dev@gmail.com>
2023-01-10 21:55:52 +0100

    Remove comma in print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -12,4 +12,4 @@ def print_matrix_integer(matrix=[[]]):
             if (j == len(matrix[i]) - 1):
                 print("{:d}".format(matrix[i][j]), end="\n")
                 continue
-            print("{:d}".format(matrix[i][j]), end=", ")
+            print("{:d}".format(matrix[i][j]), end=" ")
