04afbd35adb1a998d499e96d894523e0db7e3b0b
Adej0la <recursr.dev@gmail.com>
2023-01-13 01:40:03 +0100

    Use while loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if (not my_list or len(my_list) == 1):
+    if (not my_list):
         return my_list
-    for i in range((len(my_list) - 1), -1, -1):
+    i = len(my_list) - 1
+    while (i >= 0):
         print("{:d}".format(my_list[i]))
+        i -= 1
