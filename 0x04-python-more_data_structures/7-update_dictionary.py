07f09c39210ef85198a4c739df1901ec0034268f
Adej0la <recursr.dev@gmail.com>
2023-01-15 18:15:59 +0100

    Add function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary.get(key):
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+
+    return a_dictionary
